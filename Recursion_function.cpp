d1471d5d82a542208daf763c2660f658db51c6f6
Rahul Lokhande <rlokhande06244@gmail.com>
2024-07-22 21:54:45 +0530

    Queue

--- Recursion_function.cpp
+++ Recursion_function.cpp
@@ -1,4 +1,4 @@
-
+//=================== Direct Recursion ===================
 // WAP to create a factorial program
 #include <iostream>
 using namespace std;
@@ -19,19 +19,49 @@ using namespace std;
 //}
 
 //============ Fibonacirise In Recorsion ============
-static int count;
-int fibo(int n){
-	count++;
-	if(n==0 || n==1){
-		return n;
+//static int count;
+//int fibo(int n){
+//	count++;
+//	if(n==0 || n==1){
+//		return n;
+//	}
+//	else{
+//		return fibo(n-1)+fibo(n-2);
+//	}
+//}
+//int main(){
+//	for(int i=0;i<=8;i++){
+//		cout<<fibo(i)<<" ";
+//	}
+//	cout<<endl<<count;
+//}
+
+//=================== Table =====================
+////=============== Tail Recursion ================
+//void table(int n, int current){
+//	if(current>10){
+//		return;
+//	}
+//	else{
+//		cout<<n<<"*"<<current<<"="<<n*current<<endl;
+//		table(n , current+1);
+//	}
+//}
+//int main(){
+//	table(15,1);
+//}
+
+//=============== Head Recursion ================
+void table(int n, int current){
+	if(current>10){
+		return;
 	}
 	else{
-		return fibo(n-1)+fibo(n-2);
+		table(n , current+1);
+		cout<<n<<"*"<<current<<"="<<n*current<<endl;
+		
 	}
 }
 int main(){
-	for(int i=0;i<=8;i++){
-		cout<<fibo(i)<<" ";
-	}
-	cout<<endl<<count;
+	table(15,1);
 }
